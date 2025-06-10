ba1c2c016cedc5cefd30b60e37ed834abfb5cc27
MaiMeselhi <mai.meselhi@gmail.com>
2025-06-10 09:32:01 +0200

    handle login refresh

--- freshCart/src/App.jsx
+++ freshCart/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home'
@@ -11,9 +11,16 @@ import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Cart from './components/Cart/Cart'
 import NotFound from './components/NotFound/NotFound'
+import { tokenContext } from './components/Context/TokenContext';
 
 
 function App() {
+  let {setToken} =useContext (tokenContext)
+  useEffect (() => {
+    if(localStorage.getItem ("userToken")){
+      setToken(localStorage.getItem ("userToken"))
+    }
+  }, [])
 const routes = createBrowserRouter([
   {path:"",element: <LayOut/>, children:[
    {index:true,element:<Home/>},
